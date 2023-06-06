ff09bd1980c98d1cd8e3fbb9ff80618a17745d77
yanpol77 <jpabloponce@gmail.com>
2023-06-06 16:26:43 -0300

    .

--- dummy.cs
+++ dummy.cs
@@ -1,14 +1,11 @@
 public class dummy
 {
 	public int id { get; set; }
-<<<<<<< HEAD
+	public string name { get; set; }
 	public short age { get; set; }
 }
 
 public class person: dummy
 {
 	public int key { get; set; }
-=======
-	public string name { get; set; }
->>>>>>> refs/remotes/origin/main
 }
